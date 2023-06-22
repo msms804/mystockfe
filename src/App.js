0c46dcb51539c166f8353ffed55d1369fb798a86
msms804 <104570214+msms804@users.noreply.github.com>
2023-06-22 22:36:55 +0900

    Nav바 디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Main from "./pages/main";
 import Detail from "./pages/detail";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./css/App.css";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div>
-      myStock 앱 만들기 시작
+      <nav className="navbar">
+
+        <div className="navbar_logo" onClick={() => { navigate('/main') }}>
+          <h4>myStock</h4>
+        </div>
+        <ul className="navbar_menu">
+          <li>night mode</li>
+          <li>user</li>
+        </ul>
+
+      </nav>
+      <Routes>
+        <Route path="/main" element={<div><Main /></div>} />
+        <Route path="/detail" element={<div><Detail /></div>} />
+      </Routes>
     </div>);
 }
 
---
+++ src/components/stockChart.js
@@ -0,0 +1,6 @@
+function StockChart() {
+    return (<>
+        차트 컴포넌트임(이거에 그 D3.js?로 차트만들거야)
+    </>)
+}
+export default StockChart;
